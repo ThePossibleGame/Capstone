a250dd86436967b3a6db63c77862b8ccc1ad994f
dylan-bloch <dylan.bloch@vikings.berry.edu>
2025-01-23 21:13:00 -0500

    Updated buttons
    Buttons would create a popup for debugging. Removed this so they are functional links

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// Add basic interactivity to buttons
-document.getElementById('btn1').addEventListener('click', () => alert('Button 1 clicked'));
-document.getElementById('btn2').addEventListener('click', () => alert('Button 2 clicked'));
-document.getElementById('btn3').addEventListener('click', () => alert('Button 3 clicked'));
-
 const ball = document.querySelector('.ball');
 
 let ballPositionX = 100; // initial horizontal position
