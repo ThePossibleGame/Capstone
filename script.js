50dd7c4065de9fe6ff898b6793a7d5b5df69074d
dylan-bloch <dylan.bloch@vikings.berry.edu>
2025-01-23 20:31:19 -0500

    Milestone_1
    Finished the home screen and added the different pages that can be accessed from here. Also created the files for javascript and css additions

---
+++ script.js
@@ -0,0 +1,4 @@
+// Add basic interactivity to buttons
+document.getElementById('btn1').addEventListener('click', () => alert('Button 1 clicked'));
+document.getElementById('btn2').addEventListener('click', () => alert('Button 2 clicked'));
+document.getElementById('btn3').addEventListener('click', () => alert('Button 3 clicked'));
